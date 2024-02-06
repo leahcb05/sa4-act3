680520d05420c7dad8f2106788346b8c69884d44
Leah Bennie <lcbennie05@gmail.com>
2024-02-06 16:19:14 -0600

    changed code to give user hints to the number

--- guess_number.py
+++ guess_number.py
@@ -14,7 +14,9 @@ while True:
         if guess == number:
             print("Congratulations! You guessed the right number.")
             break
+        elif guess < number:
+            print("Sorry! Your guess was too low. Try again.")
         else:
-            print("Sorry! That's not the right number. Try again.")
+            print("Sorry! Your guess was too high. Try again.")
     except ValueError:
         print("Please enter a valid number or 'q' to quit.")
